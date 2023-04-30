7a2dfad35f7a67246e3a1f449d20d426829c46fa
Bruno Carlini <bruno-carlini@hotmail.com>
2023-04-30 17:52:00 -0300

    ...

--- PI-Empreendedor-RPG/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.piempreendedorrpg {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.piempreendedorrpg to javafx.fxml;
-    exports com.example.piempreendedorrpg;
-}
