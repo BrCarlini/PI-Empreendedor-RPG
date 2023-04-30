50a49738084bb98603b1027420a6dc170c6c01df
Bruno Carlini <bruno-carlini@hotmail.com>
2023-04-30 19:50:25 -0300

    Menu e as opções de exibir os creditos e instruções concluidas

---
+++ PI-Empreendedor-RPG/src/main/java/com/example/piempreendedorrpg/App.java
@@ -0,0 +1,117 @@
+package com.example.piempreendedorrpg;
+
+import java.util.Scanner;
+
+public class App {
+
+    public static void main(String[] args) {
+
+
+        menu();
+
+
+
+    }
+
+
+    public static void menu(){
+        Scanner entrada = new Scanner(System.in);
+
+        int opcao;
+
+        System.out.println("------ MENU DO JOGO ------");
+        System.out.println("1.: Instruções ");
+        System.out.println("2.: Jogar ");
+        System.out.println("3.: Créditos ");
+        System.out.println("4.: Sair ");
+        System.out.println("Escolha uma opção: ");
+        opcao = entrada.nextInt();
+
+        switch (opcao) {
+            case 1:
+                //System.out.println(" Instruções do JOGO ");
+                instrucao();
+                break;
+
+
+            case 2:
+                System.out.println("Jogar");
+                break;
+
+            case 3:
+                creditos();
+                break;
+
+            case 4:
+                System.out.println(" Saindo ");
+                break;
+
+            default:
+                System.out.println("Opção Inválida! ");
+                break;
+
+        }
+    }
+
+
+    public static void instrucao(){
+        Scanner entrada = new Scanner(System.in);
+
+        int opcao;
+
+
+
+        System.out.print("""
+                ===========================================  Instruções  ===========================================\n
+                1- Você é um jovem empreendedor e seu objetivo é levar sua empresa de garagem ao topo do mundo dos negócios.\n
+                2- Escolha sabiamente as decisões da jornada, cada caminho tomado tornara desafios distintos e cheios de aventura.\n
+                3- O jogo funciona com um sistema de pontuação, cada desafio superado acarretará numa soma que ao final,
+                 ranqueará sua posição no rank de empresas bem-sucedidas no mundo\n
+                4- A cada 2 perguntas, seu personagem passará para uma escolha de progresso randômico, podendo encarar
+                 “Bosses” visuais com desafios mais difíceis que a media do jogo (caso o Boss não seja derrotado, 
+                 você sofrera uma penalização de pontos por uma rodada, caso seja derrotado, um acréscimo de pontuação\n.\s
+                - Divirta se
+                ====================================================================================================\n\n""");
+
+        System.out.print("Deseja retornar ao Menu[1] ou Sair[2] ? ");
+        opcao = entrada.nextInt();
+
+        if(opcao == 1){
+            menu();
+        }else {
+            System.out.print("Saindo do programa");
+        }
+
+    }
+
+
+    public static void creditos(){
+
+        Scanner entrada = new Scanner(System.in);
+
+        int opcao;
+
+
+
+        System.out.println("------------ Créditos ------------");
+        System.out.println("CENTRO UNIVERSITÁRIO SENAC\n" +
+                "Materia: Projeto Integrador I - Desenvolvimento de Lógica\n" +
+                "Professor: Eduardo Takeo Ueda\n" +
+                "==========================================\n"+
+                "Projeto: Empreendedor RPG\n" +
+                "Devs: Bruno Carlini, Gabriel Oliveira dos Santos, João Victor Baldusco, Lucas Andrade de Sousa.\n");
+
+
+        System.out.print("Deseja retornar ao Menu[1] ou Sair[2] ? ");
+        opcao = entrada.nextInt();
+
+        if(opcao == 1){
+            menu();
+        }else {
+            System.out.print("Saindo do programa");
+        }
+
+    }
+
+}
+
